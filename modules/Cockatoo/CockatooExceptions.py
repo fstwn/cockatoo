7cf3b3739fd380f5d1f695d581aedbf527171416
fstwn <post@maxeschenbach.com>
2020-04-13 21:03:28 +0200

    module for cockatoo specific exceptions and errors

---
+++ modules/Cockatoo/CockatooExceptions.py
@@ -0,0 +1,30 @@
+
+class CockatooException(Exception):
+    """Base class for exceptions in Cockatoo."""
+
+class KnitNetworkError(CockatooException):
+    """Exception for a serious error in Cockatoo."""
+
+class MappingNetworkError(KnitNetworkError):
+    """
+    Exception raised by methods relying on a mapping network if no mapping
+    network has been assigned to the current KnitNetwork instance yet.
+    """
+
+class NoWeftEdgesError(KnitNetworkError):
+    """
+    Exception raised by methods relying on 'weft' edges if there are no 'weft'
+    edges in the network.
+    """
+
+class NoWarpEdgesError(KnitNetworkError):
+    """
+    Exception raised by methods relying on 'warp' edges if there are no 'warp'
+    edges in the network.
+    """
+
+class NoEndNodesError(KnitNetworkError):
+    """
+    Exception raised by methods relying on 'end' nodes if there are no 'end'
+    nodes in the network.
+    """
