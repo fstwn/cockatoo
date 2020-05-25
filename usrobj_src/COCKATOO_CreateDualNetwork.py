56bb4a07bed2d03d628dbf7d2b6758065c16f3f5
fstwn <post@maxeschenbach.com>
2020-05-25 21:57:16 +0200

    updated implementation

--- usrobj_src/COCKATOO_CreateDualNetwork.py
+++ usrobj_src/COCKATOO_CreateDualNetwork.py
@@ -2,7 +2,8 @@
 Create the dual network of a KnitNetwork
     Inputs:
         Toggle: Set to True to activate the component. {item, boolean}
-        KnitNetwork: The KnitNetwork to create the dual network to. {item, KnitNetwork}
+        KnitNetwork: The KnitNetwork to create the dual network to.
+                     {item, KnitNetwork}
         CyclesMode: Determines how the neighbors of each node are sorted when
                     finding the cycles of the network.
                     [-1] equals to using the world XY plane (default)
@@ -16,11 +17,12 @@ Create the dual network of a KnitNetwork
                        the origin nodes closest point on the geometrybase.
                     Defaults to [-1]. {item, int}
     Outputs:
-        DualNetwork: The dual network of the input KnitNetwork. {item, KnitNetwork}
+        DualNetwork: The dual network of the input KnitNetwork.
+                     {item, KnitNetwork}
     Remarks:
         Author: Max Eschenbach
         License: Apache License 2.0
-        Version: 200506
+        Version: 200525
 """
 
 # PYTHON STANDARD LIBRARY IMPORTS
@@ -32,6 +34,13 @@ import Grasshopper, GhPython
 import System
 import Rhino
 import rhinoscriptsyntax as rs
+
+# THIRD PARTY MODULE IMPORTS
+from ghpythonlib import treehelpers as th
+import networkx as nx
+
+# LOCAL MODULE IMPORTS
+from Cockatoo.Exceptions import KnitNetworkTopologyError
 
 # GHENV COMPONENT SETTINGS
 ghenv.Component.Name = "CreateDualNetwork"
@@ -60,9 +69,41 @@ class CDN(component):
         Dual = Grasshopper.DataTree[object]()
         
         if Toggle and KnitNetwork:
-            # create mesh from knitnetwork
+            
+            # CREATE DUAL ------------------------------------------------------
+            
             Dual = KnitNetwork.CreateDual(mode=CyclesMode)
             
-            return Dual
+            # CREATE CSV DATA (ROWS AND COLUMNS) -------------------------------
+            try:
+                raw_rows, rows = Dual.MakeCsvData()
+            except Exception as e:
+                print(e)
+                return Dual, None, None, None
+            
+            # CONVERT CSV DATA TO COMMANDS -------------------------------------
+            
+            cmd_rows = []
+            for i, row in enumerate(rows):
+                cmd_row = []
+                for node in row:
+                    if node == -1:
+                        cmd_row.append("W")
+                    else:
+                        if Dual.node[node]["end"]:
+                            cmd_row.append("E")
+                        elif Dual.node[node]["increase"]:
+                            cmd_row.append("I")
+                        elif Dual.node[node]["decrease"]:
+                            cmd_row.append("D")
+                        else:
+                            cmd_row.append("S")
+                cmd_rows.append(cmd_row)
+            
+            # CONVERT AN RETURN ------------------------------------------------
+            
+            StrOut = th.list_to_tree(cmd_rows)
+            
+            return Dual, th.list_to_tree(rows), StrOut, cmd_rows
         else:
-            return Dual
+            return Dual, None, None, None
