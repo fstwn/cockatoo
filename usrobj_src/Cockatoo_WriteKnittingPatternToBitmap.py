c84fd847727e7622811eebd832a845634dd65466
fstwn <post@maxeschenbach.com>
2020-06-16 15:23:37 +0200

    update category, remove pixelation and just write pixels

--- usrobj_src/Cockatoo_WriteKnittingPatternToBitmap.py
+++ usrobj_src/Cockatoo_WriteKnittingPatternToBitmap.py
@@ -11,7 +11,7 @@ https://discourse.mcneel.com/t/write-a-bitmap-pixel-by-pixel-in-python/81116/2
     Remarks:
         Author: Max Eschenbach
         License: Apache License 2.0
-        Version: 200603
+        Version: 200615
 """
 
 # PYTHON STANDARD LIBRARY IMPORTS
@@ -29,18 +29,19 @@ import rhinoscriptsyntax as rs
 ghenv.Component.Name = "WriteKnittingPatternToBitmap"
 ghenv.Component.NickName ="WKPTB"
 ghenv.Component.Category = "Cockatoo"
-ghenv.Component.SubCategory = "8 Pattern Data"
+ghenv.Component.SubCategory = "09 Pattern Data"
 
 class WriteKnittingPatternToBitmap(component):
     
-    def RunScript(self, Write, PatternData, Path):
+    def RunScript(self, Write, PixelData, Path):
         
-        if Write and PatternData and Path:
-            # reverse pattern data so that the start is at the bottom of the image
-            PatternData.reverse()
+        if Write and PixelData and Path:
+            # reverse the data so that the start is at the bottom of the image
+            PixelData.reverse()
+            
             # Get number of columns and rows in csv data
-            columns = len(PatternData[0])
-            rows = len(PatternData)
+            columns = len(PixelData[0])
+            rows = len(PixelData)
             
             # initialize empty bitmap
             bitmap = System.Drawing.Bitmap(columns, rows)
@@ -48,34 +49,23 @@ class WriteKnittingPatternToBitmap(component):
             # add pixels
             for i in range(columns):
                 for j in range(rows):
-                    # Get data in cell
-                    try:
-                        cd = PatternData[j][i]
-                    except IndexError:
-                        cd = "XXXXX"
                     
-                    # Make color depending on cell data
-                    if cd in ["k", "K"]:
-                        col = System.Drawing.Color.White
-                    elif cd in ["e", "E"]:
-                        col = System.Drawing.Color.Blue
-                    elif cd in ["i", "I"]:
-                        col = System.Drawing.Color.Red
-                    elif cd in ["d", "D"]:
-                        col = System.Drawing.Color.DarkRed
-                    elif cd in ["o", "O"]:
+                    try:
+                        col = PixelData[j][i]
+                    except IndexError:
                         col = System.Drawing.Color.Gray
-                    elif cd == "XXXXX":
-                        col = System.Drawing.Color.Black
-                    # Set pixel
+                    
+                    # set pixel
                     bitmap.SetPixel(i, j, col)
                     
-            # Save to file
-            bitmap.Save(path.normpath(Path.strip("\n\r")), System.Drawing.Imaging.ImageFormat.Bmp)
+            # save to file
+            bitmap.Save(path.normpath(Path.strip("\n\r")),
+                        System.Drawing.Imaging.ImageFormat.Bmp)
+        
         else:
-            if not PatternData:
+            if not PixelData:
                 rml = self.RuntimeMessageLevel.Warning
-                self.AddRuntimeMessage(rml, "No PatternData input!")
+                self.AddRuntimeMessage(rml, "No PixelData input!")
             if not Path:
                 rml = self.RuntimeMessageLevel.Warning
                 self.AddRuntimeMessage(rml, "No Path input!")
